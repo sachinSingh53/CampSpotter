6658d865d3691cb1b405d112d8abff2cf9b3c3fa
sachinSingh53 <20it095@jssaten.ac.in>
2023-09-26 23:51:07 +0530

    minor change of env file

--- app.js
+++ app.js
@@ -27,9 +27,10 @@ const usersRoutes = require('./routes/user');
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+const dbUrl = process.env.DB_URL;
 
 // 'mongodb://127.0.0.1:27017/yelp-camp'
+
 
 mongoose.connect(dbUrl,{
     // useNewUrlParser: true,
